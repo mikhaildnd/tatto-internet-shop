64d34348a8927ab29ee916327b7b00ddf5f61052
Mixxmaster <dundukovmi@gmail.com>
2022-02-15 21:51:58 +0300

    replace browserlist from gulpfile to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -132,7 +132,6 @@ export const styles = () => {
       .pipe(
         autoprefixer({
           grid: true,
-          overrideBrowserslist: ['last 2 versions', 'ie >= 11'],
           cascade: true,
         })
       )
